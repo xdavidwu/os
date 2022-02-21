3b67057ed9ac974a67261dfd90a5ce71e56ca52b
xdavidwu <xdavidwuph@gmail.com>
2022-02-21 20:40:17 +0800

    cio: cgets: handle backspace

--- console/cio.c
+++ console/cio.c
@@ -10,23 +10,32 @@ void cputs(const struct console *con, const char *str) {
 char cgetc(const struct console *con) {
 	char c = con->impl->getc();
 	c = c == '\r' ? '\n' : c; // should we really do this?
-	if (con->echo) {
-		con->impl->putc(c);
-	}
 	return c;
 }
 
 size_t cgets(const struct console *con, char *str, size_t sz) {
 	int i = 0;
-	for (; i < sz; i++) {
+	while (i < sz) {
 		char c = cgetc(con);
 		if (c == '\0') {
 			break;
+		} else if (c == '\x7f') { // DEL
+			if (i && con->echo) {
+				cputs(con, "\b \b");
+			}
+			if (i > 0) {
+				i--;
+			}
+			continue;
 		} else if (c == '\n') {
 			str[i++] = '\n';
+			con->impl->putc(c);
 			break;
 		}
-		str[i] = c;
+		if (con->echo) {
+			con->impl->putc(c);
+		}
+		str[i++] = c;
 	}
 	str[i] = '\0';
 	return i;
