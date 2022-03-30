2c3934296fee933e89eafe57aa8f1647c0c7ec23
xdavidwu <xdavidwuph@gmail.com>
2022-03-30 18:55:28 +0800

    aarch64: exceptions: unimpl: fix register mismatch

--- arch/aarch64/exceptions.c
+++ arch/aarch64/exceptions.c
@@ -18,8 +18,8 @@ static void kput64x(uint64_t val) {
 void handle_unimplemented() {
 	uint64_t spsr_el1, elr_el1, esr_el1;
 	__asm__ ("mrs %0, spsr_el1" : "=r" (spsr_el1));
-	__asm__ ("mrs %0, esr_el1" : "=r" (elr_el1));
-	__asm__ ("mrs %0, elr_el1" : "=r" (esr_el1));
+	__asm__ ("mrs %0, elr_el1" : "=r" (elr_el1));
+	__asm__ ("mrs %0, esr_el1" : "=r" (esr_el1));
 	kputs("Unimplemeted exception:\nspsr_el1:\t");
 	kput64x(spsr_el1);
 	kputs("\nelr_el1:\t");
