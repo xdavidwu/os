c7e55d98e48314f8c54eab9117cd7d9d7b47a0f7
xdavidwu <xdavidwuph@gmail.com>
2022-04-28 17:59:07 +0800

    aarch64: exceptions: unimpl: make sure it prints by sync

--- arch/aarch64/exceptions.c
+++ arch/aarch64/exceptions.c
@@ -13,6 +13,13 @@ int in_exception = 0;
 
 extern void exec_signal_handler(void *stack, void (*code)(int), uint64_t *sp, int signal);
 
+static void kputssync(const char *str) {
+	while (*str) {
+		kconsole->impl->putc(*str);
+		str++;
+	}
+}
+
 static void kput64x(uint64_t val) {
 	char hexbuf[17];
 	hexbuf[16] = '\0';
@@ -21,7 +28,7 @@ static void kput64x(uint64_t val) {
 		hexbuf[15 - i] = (dig > 9 ? 'a' - 10 : '0') + dig;
 		val >>= 4;
 	}
-	kputs(hexbuf);
+	kputssync(hexbuf);
 }
 
 void handle_unimplemented() {
@@ -30,13 +37,13 @@ void handle_unimplemented() {
 	__asm__ ("mrs %0, spsr_el1" : "=r" (spsr_el1));
 	__asm__ ("mrs %0, elr_el1" : "=r" (elr_el1));
 	__asm__ ("mrs %0, esr_el1" : "=r" (esr_el1));
-	kputs("Unimplemeted exception:\nspsr_el1:\t");
+	kputssync("Unimplemeted exception:\nspsr_el1:\t");
 	kput64x(spsr_el1);
-	kputs("\nelr_el1:\t");
+	kputssync("\nelr_el1:\t");
 	kput64x(elr_el1);
-	kputs("\nesr_el1:\t");
+	kputssync("\nesr_el1:\t");
 	kput64x(esr_el1);
-	kputc('\n');
+	kputssync("\n");
 	in_exception = 0;
 }
 
